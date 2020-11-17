638b8e63e718f651c9b3a0f58731e06a23e965fa
herteldenhoca <mehmetfatihatayoglu@gmail.com>
2020-11-17 20:51:05 +0300

    ikinci commit'

--- src/ilkGun/BirinciGun.java
+++ src/ilkGun/BirinciGun.java
@@ -3,8 +3,8 @@ package ilkGun;
 public class BirinciGun {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("11.05- Birinci Gun");
+System.out.println("ikinci Commit");
 	}
 
 }
