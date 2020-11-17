21f2aaab1728ec1a6c99e6e863cd4abe430bd3bc
herteldenhoca <74596561+herteldenhoca@users.noreply.github.com>
2020-11-17 22:14:27 +0300

    Update BirinciGun.java

--- src/ilkGun/BirinciGun.java
+++ src/ilkGun/BirinciGun.java
@@ -4,7 +4,8 @@ public class BirinciGun {
 
 	public static void main(String[] args) {
 		System.out.println("11.05- Birinci Gun");
-System.out.println("ikinci Commit");
+                System.out.println("ikinci Commit");
+		System.out.println("10.11 Ikinci Gun")
 	}
 
 }
