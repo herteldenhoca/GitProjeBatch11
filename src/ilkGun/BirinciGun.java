d51b89933be75472ec2ad5062e627716c8ed3e50
herteldenhoca <mehmetfatihatayoglu@gmail.com>
2020-11-17 22:28:24 +0300

    Ikinci gun 2. commit

--- src/ilkGun/BirinciGun.java
+++ src/ilkGun/BirinciGun.java
@@ -4,8 +4,9 @@ public class BirinciGun {
 
 	public static void main(String[] args) {
 		System.out.println("11.05- Birinci Gun");
-                System.out.println("ikinci Commit");
+        System.out.println("ikinci Commit");
 		System.out.println("10.11 Ikinci Gun");
+	    System.out.println("10:26- Ikinci Gun");
 	}
 
 }
