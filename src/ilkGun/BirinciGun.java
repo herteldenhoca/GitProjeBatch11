89ccaa254b2ba8a81bb60e73f0a44d059c27e256
herteldenhoca <mehmetfatihatayoglu@gmail.com>
2020-11-18 23:27:25 +0300

    feature

--- src/ilkGun/BirinciGun.java
+++ src/ilkGun/BirinciGun.java
@@ -14,6 +14,7 @@ System.out.println("Yepyeni bir satir");
 System.out.println("10.26 feature branch");
 System.out.println("22.56 yeni gorev feature branch");
 System.out.println("23.06 Yeni gorev mastere ekle feature da ne oldu");
+System.out.println("23.26 github ekleme zamani");
 	}
 
 }
